2c792c52b570635f3ea3b5d471dc9a3efc73b106
glh1011 <2101317923@qq.com>
2019-08-08 16:33:46 +0800

    record&report

--- utils/util.js
+++ utils/util.js
@@ -1,3 +1,5 @@
+import * as echarts from '../ec-canvas/echarts';
+
 const formatTime = date => {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
@@ -6,7 +8,8 @@ const formatTime = date => {
   const minute = date.getMinutes()
   const second = date.getSeconds()
 
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+  // return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+  return formatNumber(year)+"-"+formatNumber(month)+"-"+formatNumber(day);
 }
 
 const formatNumber = n => {
@@ -14,6 +17,31 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+const initChart = (canvas, width, height, option, element) => {
+  const chart = echarts.init(canvas, null, {
+    width: width,
+    height: height
+  });
+  canvas.setChart(chart);
+  chart.setOption(option);
+  if(element){
+    return new Promise((resolve, reject) => {
+      chart.on('finished', () => {
+        element.canvasToTempFilePath({
+          success: res => {
+            resolve(res);
+          },
+          fail: res => {
+            reject(res);
+          }
+        })
+      })
+    })
+  }
+  // return chart;
+}
+
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  initChart: initChart
 }
